dfbdd9f32cc470bc3e2972c217ca22e86832bdea
DAMIAN_CV <137531039+Codetrax-web@users.noreply.github.com>
2026-06-19 12:43:31 -0600

    Fix iframe container style in contacto section

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ document.addEventListener('DOMContentLoaded', () => {
 contacto: `
     <section class="about-section">
         <h1>Contacto</h1>
-        <div class="about-card">
+        <div class="iframe-container" style="position: relative; width: 100%; overflow: hidden; padding-top: 150%;">
                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf14qf8De2A7mz5ainwmCZJ6DZXK3BTT4-RPn0YCfWVdZD2yg/viewform?embedded=true" 
                 width="640" height="993" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </div>
