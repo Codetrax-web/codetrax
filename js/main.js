eddc4ad1f8709bec7340423297eae7c5fdd78e97
DAMIAN_CV <137531039+Codetrax-web@users.noreply.github.com>
2026-07-08 18:11:43 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@ const renderSearch = () => `
     const views = {
      home: `
      <section class="about-section">
-        <h1 class="about-title">⚜ 匚ㄖᗪ乇ㄒ尺卂乂 ⚜</h1>
+        <h1 class="about-title">⚜匚ㄖᗪ乇ㄒ尺卂乂⚜</h1>
      ${renderSearch()}
      <div id="search-results-grid"></div> 
      <div class="social-container">Buscando...</div>
